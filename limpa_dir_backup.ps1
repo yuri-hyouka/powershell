4c9c287d8d22ce5dbf0b2eda614d1b593e06ead0
Yuri Ferreira <ti.yuri.p@gmail.com>
2022-12-30 10:27:39 -0300

    Script para limpar arquivos de backup SQL SERVER do diretório

---
+++ limpa_dir_backup.ps1
@@ -0,0 +1,23 @@
+﻿$nDays = 15
+$Extensions = "*.bak"
+
+[String[]]$dias = "Friday", "Monday", "Thursday", "Tuesday", "Wednesday"
+[String[]]$periodos = "dia", "noite"
+
+foreach ($dia in $dias){
+
+    foreach ($periodo in $periodos){
+    
+        $logPath =  "D:\Backup\Financiar\$dia\$periodo"
+        $Files = Get-Childitem $LogPath -Include $Extensions -Recurse | Where {$_.LastWriteTime -le (Get-Date).AddDays(-$nDays)}
+        foreach ($File in $Files) 
+            {
+                if ($File -ne $NULL)
+                {
+                    write-host $File.FullName -ForegroundColor "DarkRed"
+                    Remove-Item $File.FullName | out-null
+            	}
+            }               
+    }
+    
+}
